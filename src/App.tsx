932dd82c20634f2b7557906d0fcbcaab5a18c13a
Saulo <saulo.castilhos@elevenfinancial.com>
2021-06-04 20:46:41 -0300

    fix: clean project

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyle from './styles/GlobalStyle'
+import Routes from './routes'
+
+import useTheme from './hooks/useTheme'
 
 function App() {
+  const { switchTheme, theme } = useTheme()
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Routes />
+      {/*  <button
+        type='button'
+        onClick={() => {
+          switchTheme()
+        }}
+      >
+        Mudar Tema
+      </button> */}
+    </ThemeProvider>
   )
 }
 
